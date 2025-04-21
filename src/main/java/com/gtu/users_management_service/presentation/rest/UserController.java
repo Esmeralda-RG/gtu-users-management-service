dcc8918e8f0d950108874b4f40b24a47516a0ec8
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-04-21 15:10:21 -0500

    Remove unnecessary RequestMapping annotation from UserController

--- src/main/java/com/gtu/users_management_service/presentation/rest/UserController.java
+++ src/main/java/com/gtu/users_management_service/presentation/rest/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +24,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/users")
 @Tag(name = "Users", description = "Endpoints for managing users")
 @CrossOrigin(origins = "*")
 public class UserController {
