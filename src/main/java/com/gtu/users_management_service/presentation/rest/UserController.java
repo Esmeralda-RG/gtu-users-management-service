651431ae9984b88933b117e88404e64c4b6263ac
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-04-21 15:11:27 -0500

    Add RequestMapping annotation to UserController for user management endpoints

--- src/main/java/com/gtu/users_management_service/presentation/rest/UserController.java
+++ src/main/java/com/gtu/users_management_service/presentation/rest/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/users")
 @Tag(name = "Users", description = "Endpoints for managing users")
 @CrossOrigin(origins = "*")
 public class UserController {
