8ee4ccfcfbd35b84944489b81c8761d194c5c4bf
SantiReRo <santiago.reyes.rodriguez@correounivalle.edu.co>
2025-06-21 10:32:40 -0500

    Add missing @MockBean annotation for PassengerService in PassengerInternalControllerTest

--- src/test/java/com/gtu/users_management_service/presentation/rest/internal/PassengerInternalControllerTest.java
+++ src/test/java/com/gtu/users_management_service/presentation/rest/internal/PassengerInternalControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -22,6 +23,7 @@ class PassengerInternalControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
     private PassengerService passengerService;
 
     @Test
