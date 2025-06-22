ceea9170c0e74ef32a93b85caee521162eac541c
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-22 18:21:59 -0500

    Add POST method to PassengerInternalController for creating new passengers

--- src/main/java/com/gtu/users_management_service/presentation/rest/internal/PassengerInternalController.java
+++ src/main/java/com/gtu/users_management_service/presentation/rest/internal/PassengerInternalController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gtu.users_management_service.application.dto.PassengerDTO;
 import com.gtu.users_management_service.domain.model.Passenger;
 import com.gtu.users_management_service.domain.service.PassengerService;
 
@@ -11,6 +12,9 @@ import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -36,4 +40,16 @@ public class PassengerInternalController {
         passenger.setId(id);
         return passengerService.resetPassword(passenger, newPassword);
     }
+
+
+    @PostMapping
+    public Passenger postMethodName(@RequestBody PassengerDTO passengerDTO) {
+        return passengerService.createPassenger(new Passenger(
+            passengerDTO.getId(),
+            passengerDTO.getName(),
+            passengerDTO.getEmail(),
+            passengerDTO.getPassword()
+        ));
+    }
+    
 }
