898b4b41f404602ed1e0262db9b678561246e81b
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-23 22:45:06 -0500

    Remove CrossOrigin annotation from PassengerController and UserController

--- src/main/java/com/gtu/users_management_service/presentation/rest/PassengerController.java
+++ src/main/java/com/gtu/users_management_service/presentation/rest/PassengerController.java
@@ -1,7 +1,6 @@
 package com.gtu.users_management_service.presentation.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/passengers")
 @Tag(name = "Passenger", description = "Endpoints for managing passengers")
-@CrossOrigin(origins = "*")
 public class PassengerController {
     
     private final PassengerUseCase passengerUseCase;
